fb06de55f0877d16f64e0ce242de10104b0c879b
Carolyn Van Slyck <me@carolynvanslyck.com>
2016-07-07 15:42:34 -0500

    debug info

--- myget.ps1
+++ myget.ps1
@@ -8,6 +8,7 @@ if(!$env:PATH.Contains($gobin)) { $env:PATH+=";$gobin" }
 echo "Moving source code into the GOPATH..."
 mkdir -f src/$package > $null
 ls -exclude @("src","myget.ps1") | %{mv $_.fullname src\$package}
+ls src\$package
 
 echo "Running build script..."
 bash -c "src/$package/build.sh"
