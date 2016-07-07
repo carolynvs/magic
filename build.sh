7b1e5d9c170c7e8bfa6d4c5da03b068b42940e59
Carolyn Van Slyck <me@carolynvanslyck.com>
2016-07-07 15:41:10 -0500

    Print go version

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ go get github.com/Masterminds/glide
 $( cd "$( dirname "${BASH_SOURCE[0]}" )" && glide install)
 
 # Let's make some magic
-echo "Building all targets..."
+echo "Building all targets with $(go version)"
 GOOS=linux GOARCH=amd64 go build -o bin/Linux/x86_64/magic $PACKAGE
 GOOS=darwin GOARCH=amd64 go build -o bin/Darwin/x86_64/magic $PACKAGE
 GOOS=windows GOARCH=amd64 go build -o bin/Windows/x86_64/magic.exe $PACKAGE
