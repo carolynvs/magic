ae2a4ae80f75447f09dea3d2ddfab30f064d5a18
Carolyn Van Slyck <me@carolynvanslyck.com>
2016-07-08 10:38:52 -0500

    Use git for windows sdk

--- myget.ps1
+++ myget.ps1
@@ -1,20 +1,22 @@
-$package = "github.com/carolynvs/magic"
+$packagePath = "src/github.com/carolynvs/magic"
+$ErrorActionPreference = "SilentlyContinue"
 
 echo "Configuring the GOPATH and GOBIN..."
 $env:GOPATH = "$PSScriptRoot"
-$gobin = "$env:GOPATH\bin"
-if(!$env:PATH.Contains($gobin)) { $env:PATH+=";$gobin" }
+$env:PATH+=";$env:GOPATH\bin"
 
 echo "Moving source code into the GOPATH..."
-mkdir -f src/$package > $null
-ls -exclude @("src","myget.ps1") | %{mv $_.fullname src\$package}
-ls src\$package
+mkdir -f $packagePath *> $null
+ls -exclude @("src","myget.ps1") | %{mv $_.fullname $packagePath}
+cd $packagePath
 
-echo "Go information:"
-(Get-Command go).definition
-go version
-go env
+# Copy the Git for Windows bash wrapper into the Git for Windows SDK
+$bash = "C:\git-sdk-64\bin\bash.exe"
+mkdir -f (split-path $bash) *> $null
+if(!(Test-Path $bash)) { cp "C:\Program Files (x86)\Git\bin\bash.exe" $bash }
 
-echo "Running build script..."
-cd src/$package
-bash -c "./build.sh"
+echo "Running bash script..."
+& $bash -c "./build.sh"
+
+echo "Running make script..."
+& $bash -c "make"
