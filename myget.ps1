09039e3521ddf98268a705732880412ff2b7fdf2
Carolyn Van Slyck <me@carolynvanslyck.com>
2016-07-08 16:12:39 -0500

    Use downloaded bash.exe

--- myget.ps1
+++ myget.ps1
@@ -1,7 +1,7 @@
 $packagePath = "src/github.com/carolynvs/magic"
 $ErrorActionPreference = "SilentlyContinue"
 
-echo "Configuring the GOPATH and GOBIN..."
+echo "Configuring GOPATH and GOBIN..."
 $env:GOPATH = "$PSScriptRoot"
 $env:PATH+=";$env:GOPATH\bin"
 
@@ -10,28 +10,13 @@ mkdir -f $packagePath *> $null
 ls -exclude @("src","myget.ps1") | %{mv $_.fullname $packagePath}
 cd $packagePath
 
-# Get the Git for Windows bash.exe wrapper
+echo "Downloading the Git for Windows bash.exe wrapper..."
 $bash = "C:\git-sdk-64\bin\bash.exe"
 mkdir -f (split-path $bash) *> $null
 curl -outfile $bash https://bintray.com/carolynvs/git-for-windows-tools/download_file?file_path=bash-x86.exe
 
-#echo "Running bash script..."
-#& $bash -c "./build.sh"
+echo "Running bash script..."
+& $bash -c "./build.sh"
 
-echo "Looking at the env"
-ls 'c:\'
-ls 'c:\git'
-ls 'c:\Program Files'
-ls 'c:\Program Files (x86)'
-& $bash -c "set -x; uname -a"
-& $bash -c "set -x; echo `$PWD"
-& $bash -c "set -x; echo `echo `$PATH"
-& $bash -c "set -x; which make"
-& $bash -c "set -x; which go"
-& $bash -c "set -x; ls /usr/bin | grep make"
-& $bash -c "set -x; ls /bin | grep make"
-c:\git-sdk-64\usr\bin\bash.exe --login -i -c "set -x; ls /usr/bin | grep make"
-c:\git-sdk-64\usr\bin\bash.exe --login -i -c "set -x; ls /bin | grep make"
-c:\git-sdk-64\usr\bin\bash.exe --login -i -c "set -x; which pacman"
-#echo "Running make script..."
-#& $bash -c "/usr/bin/make"
+echo "Running make script..."
+& $bash -c "make"
