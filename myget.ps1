d2e6bb123907cb4d352909d9a4ce19822212a549
Carolyn Van Slyck <me@carolynvanslyck.com>
2016-07-08 16:09:36 -0500

    Download bash.exe

--- myget.ps1
+++ myget.ps1
@@ -10,10 +10,10 @@ mkdir -f $packagePath *> $null
 ls -exclude @("src","myget.ps1") | %{mv $_.fullname $packagePath}
 cd $packagePath
 
-# Copy the Git for Windows bash wrapper into the Git for Windows SDK
+# Get the Git for Windows bash.exe wrapper
 $bash = "C:\git-sdk-64\bin\bash.exe"
 mkdir -f (split-path $bash) *> $null
-if(!(Test-Path $bash)) { cp "C:\Program Files (x86)\Git\bin\bash.exe" $bash }
+curl -outfile $bash https://bintray.com/carolynvs/git-for-windows-tools/download_file?file_path=bash-x86.exe
 
 #echo "Running bash script..."
 #& $bash -c "./build.sh"
