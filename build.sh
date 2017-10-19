beee28f12e803df7abafaa370be226bb4a26cf44
Carolyn Van Slyck <me@carolynvanslyck.com>
2017-10-19 13:25:47 -0500

    Remove glide from appveyor script

--- build.sh
+++ build.sh
@@ -3,11 +3,6 @@ set -e
 
 PACKAGE="github.com/carolynvs/magic"
 
-# Get our magical dependencies
-echo "Installing glide..."
-go get github.com/Masterminds/glide
-glide install 2>&1
-
 # Let's make some magic
 echo "Building all targets with $(go version)"
 GOOS=linux GOARCH=amd64 go build -o bin/Linux/x86_64/magic $PACKAGE
